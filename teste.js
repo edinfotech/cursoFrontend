b75ab8e4ee718a5715a1bce356ac1fedb8abdc31
edivanio <edinfosuporte@gmail.com>
2018-11-01 07:07:11 -0300

    git edi

---
+++ teste.js
@@ -0,0 +1,40 @@
+
+{  
+   "OrderNumber":"1",
+   "SoftDescriptor":"Manari",
+   "Cart":{  
+      
+      "Items":[  
+         {  
+            "Name":"investir",
+            "Description":"investir",
+            "UnitPrice":"100",
+            "Quantity":"1",
+            "Type":"Service"
+            
+         }
+         ]
+   },
+      "Shipping":{  
+      "SourceZipCode":"",
+      "TargetZipCode":"",
+      "Type":"Free"
+   
+      },
+ 
+   "Customer":{  
+      
+      "FullName":"edi",
+      "Email":""
+      
+   },
+   "Options":{  
+     "AntifraudEnabled":true,
+     "ReturnUrl": "https://manariconstrucaoereforma.com.br/finalizar-compra/order-received/"
+   },
+   "Settings":{
+        "CheckoutUrl": "https://cieloecommerce.cielo.com.br/transacional/order/index?id=123",
+        "Profile": "CheckoutCielo",
+        "Version": 1
+    }
+}
